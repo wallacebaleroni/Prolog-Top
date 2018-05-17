8cde08793e1bc68b712d677e8446531b93a33389
Júlia Falcão <juliafsfalcao@hotmail.com>
2018-05-17 10:15:02 -0300

    apelidos e copas

---
+++ selecao.pl
@@ -0,0 +1,57 @@
+apelido(selecao, "seleção verde-amarela").
+apelido(selecao, "seleção canarinho").
+apelido(selecao, "seleção amarelinha").
+
+participou(selecao, copadomundo, 1930).
+participou(selecao, copadomundo, 1934).
+participou(selecao, copadomundo, 1938).
+participou(selecao, copadomundo, 1950).
+participou(selecao, copadomundo, 1954).
+participou(selecao, copadomundo, 1958).
+participou(selecao, copadomundo, 1962).
+participou(selecao, copadomundo, 1966).
+participou(selecao, copadomundo, 1970).
+participou(selecao, copadomundo, 1974).
+participou(selecao, copadomundo, 1978).
+participou(selecao, copadomundo, 1982).
+participou(selecao, copadomundo, 1986).
+participou(selecao, copadomundo, 1990).
+participou(selecao, copadomundo, 1994).
+participou(selecao, copadomundo, 1998).
+participou(selecao, copadomundo, 2002).
+participou(selecao, copadomundo, 2006).
+participou(selecao, copadomundo, 2010).
+participou(selecao, copadomundo, 2014).
+
+ganhou(selecao, copadomundo, 1958).
+ganhou(selecao, copadomundo, 1962).
+ganhou(selecao, copadomundo, 1970).
+ganhou(selecao, copadomundo, 1994).
+ganhou(selecao, copadomundo, 2002).
+
+participou(p) :- ganhou(p).
+
+ganhou(selecao, copadasamericas, 1919).
+ganhou(selecao, copadasamericas, 1922).
+ganhou(selecao, copadasamericas, 1949).
+ganhou(selecao, copadasamericas, 1989).
+ganhou(selecao, copadasamericas, 1997).
+ganhou(selecao, copadasamericas, 1999).
+ganhou(selecao, copadasamericas, 2004).
+ganhou(selecao, copadasamericas, 2007).
+
+
+ganhou(selecao, copadasconfederacoes, 1997).
+ganhou(selecao, copadasconfederacoes, 2005).
+ganhou(selecao, copadasconfederacoes, 2009).
+ganhou(selecao, copadasconfederacoes, 2013).
+
+ganhou(selecao, olimpiadas, 2016).
+
+sede(olimpiadas, 2016, riodejaneiro).
+sede(copadomundo, 1958, suecia).
+sede(copadomundo, 1962, chile).
+sede(copadomundo, 1970, mexico).
+sede(copadomundo, 1994, eua).
+sede(copadomundo, 2002, coreiadosul).
+sede(copadomundo, 2002, japao).
