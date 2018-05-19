a593d2a0d011305152c67a649589f0bb48e19556
Júlia Falcão <juliafsfalcao@hotmail.com>
2018-05-18 22:22:02 -0300

    organização, jogadores, clubes, lugares, regras
    botei coisa à beça

--- selecao.pl
+++ selecao.pl
@@ -6,8 +6,19 @@ apelido(brasil, "seleção verde-amarela").
 apelido(brasil, "seleção canarinho").
 apelido(brasil, "seleção amarelinha").
 
-participou(p) :- ganhou(p).
+/* selecao(PAIS) */
+selecao(brasil).
+selecao(alemanha).
+selecao(nicaragua).
+selecao(uruguai).
+selecao(argentina).
+selecao(suecia).
+selecao(italia).
+selecao(franca).
 
+competicao(COMPETICAO) :- participou(_, COMPETICAO, _).
+
+/* participou(SELECAO, COMPETICAO, ANO) */
 participou(brasil, copadomundo, 1930).
 participou(brasil, copadomundo, 1934).
 participou(brasil, copadomundo, 1938).
@@ -28,19 +39,32 @@ participou(brasil, copadomundo, 2002).
 participou(brasil, copadomundo, 2006).
 participou(brasil, copadomundo, 2010).
 participou(brasil, copadomundo, 2014).
-
 participou(alemanha, copadomundo, 2014).
 
-todascopas(PAIS) :- aggregate_all(count, participou(PAIS,copadomundo,_), Count),Count = 20.
+participou(SELECAO, COMPETICAO, ANO) :- ganhou(SELECAO, COMPETICAO, ANO).
 
+/* ganhou(SELECAO, COMPETICAO, ANO) */
 ganhou(brasil, copadomundo, 1958).
 ganhou(brasil, copadomundo, 1962).
 ganhou(brasil, copadomundo, 1970).
 ganhou(brasil, copadomundo, 1994).
 ganhou(brasil, copadomundo, 2002).
 
+ganhou(alemanha, copadomundo, 1954).
+ganhou(alemanha, copadomundo, 1974).
+ganhou(alemanha, copadomundo, 1990).
 ganhou(alemanha, copadomundo, 2014).
-
+ganhou(italia, copadomundo, 1934).
+ganhou(italia, copadomundo, 1938).
+ganhou(italia, copadomundo, 1982).
+ganhou(italia, copadomundo, 2006).
+ganhou(argentina, copadomundo, 1978).
+ganhou(argentina, copadomundo, 1986).
+ganhou(uruguai, copadomundo, 1930).
+ganhou(uruguai, copadomundo, 1950).
+ganhou(franca, copadomundo, 1998).
+ganhou(inglaterra, copadomundo, 1996).
+ganhou(espanha, copadomundo, 2010).
 
 ganhou(brasil, copadasamericas, 1919).
 ganhou(brasil, copadasamericas, 1922).
@@ -50,7 +74,6 @@ ganhou(brasil, copadasamericas, 1997).
 ganhou(brasil, copadasamericas, 1999).
 ganhou(brasil, copadasamericas, 2004).
 ganhou(brasil, copadasamericas, 2007).
-
 
 ganhou(brasil, copadasconfederacoes, 1997).
 ganhou(brasil, copadasconfederacoes, 2005).
@@ -66,16 +89,10 @@ ganhou(brasil, jogospanamericanos, 1963).
 ganhou(brasil, jogospanamericanos, 1979).
 ganhou(brasil, jogospanamericanos, 1987).
 
-pentacampeao(PAIS) :- aggregate_all(count, ganhou(PAIS,copadomundo,_), Count),Count = 5.
+todascopas(PAIS) :- aggregate_all(count, participou(PAIS, copadomundo, _), Count), Count = 20.
+pentacampeao(PAIS) :- aggregate_all(count, ganhou(PAIS, copadomundo, _), Count), Count = 5.
 
-sede(olimpiadas, 2016, riodejaneiro).
-sede(copadomundo, 1958, suecia).
-sede(copadomundo, 1962, chile).
-sede(copadomundo, 1970, mexico).
-sede(copadomundo, 1994, eua).
-sede(copadomundo, 2002, coreiadosul).
-sede(copadomundo, 2002, japao).
-
+/* fezparte(JOGADOR, SELECAO) */
 fezparte(rivellino, brasil).
 fezparte(zico, brasil).
 fezparte(ronaldo, brasil).
@@ -92,6 +109,64 @@ fezparte(bebeto, brasil).
 fezparte(rivaldo, brasil).
 fezparte(neymar, brasil).
 
+/* fazparte(JOGADOR, SELECAO) */
+/* elenco atual da seleção */
+fazparte(JOGADOR, brasil) :- posicao(JOGADOR, _, _).
+
+/* posicao(JOGADOR, NUMERO, POSICAO) */
+posicao(alisson, 1, goleiro).
+posicao(cassio, 16, goleiro).
+posicao(ederson, 23, goleiro).
+posicao(danilo, 2, lateral).
+posicao(filipeluis, 6, lateral).
+posicao(marcelo, 12, lateral).
+posicao(fagner, 22, lateral).
+posicao(miranda, 3, zagueiro).
+posicao(pedrogeromel, 4, zagueiro).
+posicao(marquinhos, 13, zagueiro).
+posicao(thiagosilva, 14, zagueiro).
+posicao(casemiro, 5, volante).
+posicao(renatoaugusto, 8, meia).
+posicao(philippecoutinho, 11, meia).
+posicao(paulinho, 15, volante).
+posicao(fernandinho, 17, volante).
+posicao(fred, 18, meia).
+posicao(willian, 19, meia).
+posicao(douglascosta, 7, atacante).
+posicao(gabrieljesus, 9, atacante).
+posicao(neymar, 10, atacante).
+posicao(taison, 20, atacante).
+posicao(robertofirmino, 21, atacante).
+
+posicao(JOGADOR, POSICAO) :- posicao(JOGADOR, _, POSICAO).
+numero(JOGADOR, NUMERO) :- posicao(JOGADOR, NUMERO, _).
+
+/* clube(JOGADOR, CLUBE) */
+cube(alisson, roma).
+clube(cassio, corinthians).
+clube(ederson, manchestercity).
+clube(danilo, manchestercity).
+clube(filipeluis, atleticodemadrid).
+clube(marcelo, realmadrid).
+clube(fagner, corinthians).
+clube(miranda, internazionale).
+clube(pedrogeromel, gremio).
+clube(marquinhos, parissaintgermain).
+clube(thiagosilva, parissaintgermain).
+clube(casemiro, realmadrid).
+clube(renatoaugusto, beijingguoan).
+clube(philippecoutinho, barcelona).
+clube(paulinho, barcelona).
+clube(fernandinho, manchestercity).
+clube(fred, shakhtardonetsk).
+clube(willian, chelsea).
+clube(douglascosta, juventus).
+clube(gabrieljesus, manchestercity).
+clube(neymar, parissaintgermain).
+clube(taison, shakhtardonetsk).
+clube(robertofirmino, liverpool).
+
+/* fornecedor(MARCA, ANO_INICIO, ANO_FIM) */
 fornecedor(athleta, 1954, 1977).
 fornecedor(adidas, 1977, 1981).
 fornecedor(topper, 1981, 1990).
@@ -100,9 +175,13 @@ fornecedor(nike, 1997, 2018).
 
 fornecedor(MARCA, ANO) :- fornecedor(MARCA, INI, FIM), ANO >= INI, ANO =< FIM.
 
+fornecedor(brasil, MARCA) :- fornecedor(MARCA, _, _).
+
+/* rival (SELECAO_1, SELECAO_2). */
 rival(brasil, argentina).
 rival(brasil, uruguai).
 
+/* titulo (TITULO, JOGADOR, ANO) */
 titulo(melhordomundo, romario, 1994).
 titulo(melhordomundo, ronaldo, 1996).
 titulo(melhordomundo, ronaldo, 1997).
@@ -111,26 +190,31 @@ titulo(melhordomundo, ronaldinhogaucho, 2004).
 titulo(melhordomundo, ronaldinhogaucho, 2005).
 titulo(melhordomundo, kaka, 2007).
 
+/* uniforme(TIPO_MEMBRO, TIPO_UNIFORME, COR_1, COR_2) */
 uniforme(jogador, principal, amarelo, azul).
 uniforme(jogador, alternativo, azul, branco).
 uniforme(goleiro, principal, verde, verde).
 uniforme(goleiro, principal, cinza, cinza).
 uniforme(goleiro, principal, preto, roxo).
-uniforme(jogador, treino, azul).
-uniforme(goleiro, treino, preto).
-uniforme(comissaotecnica, treino, preto).
+uniforme(jogador, treino, azul, azul).
+uniforme(goleiro, treino, preto, preto).
+uniforme(comissaotecnica, treino, preto, preto).
 
-tecnico(tite,2016,2018).
-tecnico(dunga,2014,2016).
-tecnico(felipao,2013,2014).
-tecnico(manomenezes,2010,2012).
-tecnico(dunga,2006,2010).
-tecnico(parreira,2003,2006).
-tecnico(felipao,2001,2002).
-tecnico(leao,2000,2001).
+/* tecnico(TECNICO, ANO_INICIO, ANO_FIM) */
+tecnico(tite, 2016, 2018).
+tecnico(dunga, 2014, 2016).
+tecnico(felipao, 2013, 2014).
+tecnico(manomenezes, 2010, 2012).
+tecnico(dunga, 2006, 2010).
+tecnico(parreira, 2003, 2006).
+tecnico(felipao, 2001, 2002).
+tecnico(leao, 2000, 2001).
 
 tecnico(NOME, ANO) :- tecnico(NOME, INI, FIM), ANO >= INI, ANO =< FIM.
 
+tecnico(brasil) :- tecnico(_, _, _).
+
+/* patrocina(MARCA, ANO_INICIO, ANO_FIM) */
 patrocina(guaranaantarctica, 2001, 2019).
 patrocina(guaranaantarctica, 2005, 2014).
 patrocina(guaranaantarctica, 2008, 2022).
@@ -145,15 +229,44 @@ patrocina(chevrolet, 2014, 2018).
 
 patrocina(MARCA, ANO) :- patrocina(MARCA, INI, FIM), ANO >= INI, ANO =< FIM.
 
+patrocina(brasil, MARCA) :- patrocina(MARCA, _, _).
+
+/* jogou(SELECAO_1, PLACAR_SELECAO_1, PLACAR_SELECAO_2,SELECAO_2, COMPETICAO, ANO) */
 jogou(brasil, 1, 7, alemanha, copadomundo, 2014).
 jogou(brasil, 14, 0, nicaragua, jogospanamericanos, 1975).
 jogou(brasil, 0, 6, uruguai, campeonatosulamericano, 1920).
 jogou(brasil, 5, 2, suecia, copadomundo, 1958).
 jogou(brasil, 4, 1, italia, copadomundo, 1970).
-jogou(brasil, 0, 3, frança, copadomundo, 1998).
+jogou(brasil, 0, 3, franca, copadomundo, 1998).
 jogou(brasil, 2, 0, alemanha, copadomundo, 2002).
+jogou(brasil, 0, 0, italia, copadomundo, 1994).
 
+jogou(SELECAO1, P1, P2, SELECAO2, COMPETICAO, ANO) :- jogou(SELECAO2, P2, P1, SELECAO1, COMPETICAO, ANO).
 ganhoupartida(brasil, ADVERSARIO, CAMPEONATO, ANO) :- jogou(brasil, BR, ADV, ADVERSARIO, CAMPEONATO, ANO), BR > ADV.
 perdeupartida(brasil, ADVERSARIO, CAMPEONATO, ANO) :- jogou(brasil, BR, ADV, ADVERSARIO, CAMPEONATO, ANO), BR < ADV.
 
+/* sede(COMPETICAO, ANO, LOCAL) */
+sede(copadomundo, 1958, suecia).
+sede(copadomundo, 1962, chile).
+sede(copadomundo, 1970, mexico).
+sede(copadomundo, 1994, eua).
+sede(copadomundo, 2002, coreiadosul).
+sede(copadomundo, 2002, japao).
+sede(olimpiadas, 2016, riodejaneiro).
 
+/* ficaem(CIDADE, PAIS)
+   ficaem(PAIS, SUBCONTINENTE).
+   ficaem(SUBCONTINENTE, CONTINENTE). */
+
+ficaem(riodejaneiro, brasil).
+ficaem(brasil, americadosul).
+ficaem(suecia, europa).
+ficaem(mexico, americadonorte).
+ficaem(chile, americadosul).
+ficaem(eua, americadonorte).
+ficaem(coreiadosul, asia).
+ficaem(japao, asia).
+ficaem(americadosul, america).
+ficaem(americadonorte, america).
+
+ficaem(X, Z) :- ficaem(X, Y), ficaem(Y, Z).
