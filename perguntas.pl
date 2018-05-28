2341055d9fffadf5fb174ab85b253861bda37e37
Raffael M. Paranhos <raffaelmp@id.uff.br>
2018-05-28 00:13:55 -0300

    6 perguntinhas

--- perguntas.pl
+++ perguntas.pl
@@ -1,14 +1,26 @@
 /*1*/
+/*A selecao brasileira participou de todas as copas? Oraculo: true*/
+todascopas(brasil).
 
 /*2*/
+/*A selecao brasileira é pentacampea mundial? Oraculo: true*/
+pentacampea(brasil).
 
 /*3*/
+/*A selecao brasileira é campea olimpica mundial? Oraculo: true*/
+campeaoolimpico(brasil).
 
 /*4*/
+/*Qual o atual tecnico da selecao brasileira? Oraculo: X = tite*/
+tecnico(X,2018).
 
 /*5*/
+/*Qual o atual fornecedor de material esportivo da selecao brasileira? Oraculo: X = nike*/
+fornecedor(X,2018).
 
 /*6*/
+/*O neymar partcipa da champions league? Oraculo: true*/
+jogana(neymar,championsleague).
 
 /*7*/
 
