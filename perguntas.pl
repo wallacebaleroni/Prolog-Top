5051852aab53204a463e1520b8930c2e5edc79ef
Júlia Falcão <juliafsfalcao@hotmail.com>
2018-06-07 20:03:43 -0300

    final

--- perguntas.pl
+++ perguntas.pl
@@ -1,28 +1,58 @@
-/*1*/
-/*A selecao brasileira participou de todas as copas? Oraculo: true*/
-todascopas(brasil).
+/*
+1. O Brasil já venceu alguma Copa do Mundo na Europa?
+*/
 
-/*2*/
-/*A selecao brasileira é pentacampea mundial? Oraculo: true*/
-pentacampea(brasil).
+?- ganhou_copa_em_continente(brasil, europa).
 
-/*3*/
-/*A selecao brasileira é campea olimpica mundial? Oraculo: true*/
-campeaoolimpico(brasil).
+/*
+2. Quem ganhou a Copa do Mundo no ano em que o Brasil perdeu uma partida por uma diferença de 6 gols?
+*/
 
-/*4*/
-/*Qual o atual tecnico da selecao brasileira? Oraculo: X = tite*/
-tecnico(X,2018).
+?- jogou(brasil, GOLS1, GOLS2, _, copadomundo, ANO), ganhou(VENCEDOR, copadomundo, ANO), 6 is P2-P1, !.
 
-/*5*/
-/*Qual o atual fornecedor de material esportivo da selecao brasileira? Oraculo: X = nike*/
-fornecedor(X,2018).
+/*
+3. A seleção já teve um mesmo técnico durante dois períodos de tempo diferentes?
+*/
+?- tecnico_2_periodos(brasil).
 
-/*6*/
-/*O neymar partcipa da champions league? Oraculo: true*/
-jogana(neymar,championsleague).
+/*
+4. Qual é a cor do uniforme que o Neymar usa nos treinos da seleção?
+*/
 
+?- posicao(neymar, _, POS), tipo_posicao(POS, TIPO), uniforme(TIPO, treino, COR1, COR2).
 
+/*
+5. A seleção brasileira participou de todas as Copas do Mundo até hoje?
+*/
 
-/* O Brasil já ganhou alguma Copa do Mundo na Europa? */
-ganhou(brasil, copadomundo, ANO), sede(copadomundo, ANO, LOCAL), fica_em(LOCAL, europa).
+?- todas_copas(brasil).
+
+/*
+6. Quem é o atual fornecedor de material esportivo da seleção brasileira?
+*/
+
+?- ano_atual(ANO), fornecedor(MARCA, ANO), !.
+
+/*
+7. Há algum jogador no elenco atual da seleção que joga em um time chinês?
+*/
+
+joga_no_time(JOGADOR, brasil), joga_no_time(JOGADOR, TIME), baseado_em(TIME, CIDADE), fica_em(CIDADE, china).
+
+/*
+8. O Neymar atualmente joga na Copa Libertadores da América?
+*/
+
+?- joga_no_campeonato(neymar, libertadores).
+
+/*
+9. Em qual time joga o camisa 15 da seleção?
+*/
+
+?- posicao(NOME, 15, _), joga_no_time(NOME, TIME), TIME \== brasil.
+
+/*
+10. Quem era o técnico da seleção na última vez que o Brasil venceu a Copa do Mundo?
+*/
+
+?- ultima_vitoria_na_copa(ANO), tecnico(NOME, ANO), !.
